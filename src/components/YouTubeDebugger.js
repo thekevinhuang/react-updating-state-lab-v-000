adfb08e1b59beb19f2fcb7faef1ff154ac2c1109
Kevin Huang <yellow.khaos@gmail.com>
2019-01-27 02:45:25 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -2,5 +2,19 @@
 import React, {Component} from 'react';
 
 export default class YouTubeDebugger extends Component {
+  constructor () {
+    super()
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {bitrate: 8, video: {resolution: '1080p'}}
+    }
+  }
 
+  render() {
+    return (
+        <button className="bitrate" onClick={this.bitHandler}>Bitrate Adjuster</button>
+        <button className="resolution" onClick={this.resHandler}>Resolution Adjuster</button>
+    )
+  }
 }
